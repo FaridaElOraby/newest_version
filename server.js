5507afb8a635f12db96bd12f3754139ed0ba0a14
FaridaElOraby <faridaeloraby@gmail.com>
2020-04-26 14:56:10 +0200

    l

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+// DB Config
+//const db = require("./config/keys").mongoURI;
+// Connect to MongoDB
+
+mongoose.connect('mongodb://localhost/testaroo');
+    mongoose.connection.once('open', function(){
+        console.log('Connection has been made, now make fireworks...');
+    }).on('error', function(error){
+        console.log('Connection error:', error);
+    });
+
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log("MongoDB successfully connected"))
+//   .catch(err => console.log(err));
+
+  // Passport middleware
+  app.use(passport.initialize());
+  // Passport config
+  require("./config/passport")(passport);
+  // Routes
+  app.use("/api/users", users);
+  
+
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
